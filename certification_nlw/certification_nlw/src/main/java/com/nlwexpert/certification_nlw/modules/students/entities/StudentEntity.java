ea8d50b232577e9ce4f849f37c56f891a67856ca
Jady Lima <jadylsilva@gmail.com>
2024-02-10 20:08:43 -0300

    Aula 03

--- certification_nlw/certification_nlw/src/main/java/com/nlwexpert/certification_nlw/modules/students/entities/StudentEntity.java
+++ certification_nlw/certification_nlw/src/main/java/com/nlwexpert/certification_nlw/modules/students/entities/StudentEntity.java
@@ -6,6 +6,9 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -35,5 +38,6 @@ public class StudentEntity {
     private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "studentEntity")
+    @JsonBackReference
     private List<CertificationStudentEntity> certificationStudentEntities;
 }
